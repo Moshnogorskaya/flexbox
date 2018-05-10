1bc3a1d3f9ce70e976ea3170500983c3fb38c46f
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-10 19:56:22 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './style.scss';
 
-let slides = document.querySelectorAll('.slider__slide');
+const slides = document.querySelectorAll('.slider__slide');
 let currentSlide = 0;
-let slideInterval = setInterval(nextSlide,3000);
-function nextSlide() {
-slides[currentSlide].className = 'slider__slide';
 
-// используем оператор % на случай, если это был последний слайд, чтобы вернуться к первому
-currentSlide = (currentSlide+1)%slides.length;
-slides[currentSlide].className = 'slider__slide showing';
+function nextSlide() {
+  slides[currentSlide].className = 'slider__slide';
+
+  // используем оператор % на случай, если это был последний слайд, чтобы вернуться к первому
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = 'slider__slide showing';
 }
+
+setInterval(nextSlide, 3000);
