5b8e8b8871a95e28505a037fb835cba0c261aa41
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-10 16:02:47 +0300

    sass installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -6,10 +7,43 @@ module.exports = {
   },
   devServer: {
     contentBase: "./dist",
-    stats: 'errors-only'
+    stats: "errors-only"
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
