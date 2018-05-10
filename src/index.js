4cfa38a4b00fda410db2291d6385cf64aa248912
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-10 19:15:48 +0300

    slideshow works

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.scss';
+
+var slides = document.querySelectorAll('.slider__slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,3000);
+function nextSlide() {
+slides[currentSlide].className = 'slider__slide';
+
+// используем оператор % на случай, если это был последний слайд, чтобы вернуться к первому
+currentSlide = (currentSlide+1)%slides.length;
+slides[currentSlide].className = 'slider__slide showing';
+}
