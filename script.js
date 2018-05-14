b8c13e241a18fb3361a22b7fc6e073c783eda8c6
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-14 14:20:04 +0300

    works without color change

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-let makeBlocks = function generateBlocksWithDataFromExtSource(paras=4) {
-  if (paras < 0 || paras > 5) paras = 0;
+let color = function changeColorOfSomeHeadings (elements){
+
+}
+
+
+
+let makeBlocks = function generateBlocksWithDataFromExtSource(paras = 4) {
+  if (paras < 0) paras = 0;
   const postTemplate = document.querySelector(".post_half");
   const postContainer = document.querySelector(".posts");
+  for (i = 0; i < paras; i += 1) {
+    let duplicate = postTemplate.cloneNode(true);
+    duplicate.classList.add("duplicate");
+    postContainer.insertBefore(duplicate, null);
+  }
+  let duplicates = postContainer.querySelectorAll(".duplicate");
+  //color(duplicates, 'yellow');
 
-  let request = new XMLHttpRequest();
-  let uRl = "https://baconipsum.com/api/?type=all-meat&paras=" + paras;
-  request.open("GET", uRl);
-  request.send();
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
+  let requestText = new XMLHttpRequest();
+  let urlText = "https://baconipsum.com/api/?type=all-meat&paras=" + paras;
+  requestText.open("GET", urlText);
+  requestText.send();
+  requestText.onreadystatechange = () => {
+    if (requestText.readyState === 4) {
       let extPage;
-      extPage = JSON.parse(request.responseText);
-
-      for (let i = 0; i < paras; i += 1) {
-        let dupPost = postTemplate.cloneNode(true);
-        let textPlaceholder = dupPost.querySelector(".post__main-text");
-        let imagePlaceholder = dupPost.querySelector('.post__image');
+      extPage = JSON.parse(requestText.responseText); // extPages
+      duplicates.forEach((duplicate, i) => {
+        let textPlaceholder = duplicate.querySelector(".post__main-text");
         textPlaceholder.innerHTML = extPage[i];
-        imagePlaceholder.style.backgroundImage = "url('https://picsum.photos/200/?random')";
-        dupPost.style.display = "flex";
-        postContainer.insertBefore(dupPost, null);
-      }
+      });
+      
+      duplicates.forEach(duplicate => {
+        let requestImage = new XMLHttpRequest();
+        let urlImage = "https://picsum.photos/200/?random";
+        requestImage.open("GET", urlImage);
+        requestImage.send();
+        requestImage.onreadystatechange = () => {
+          if (requestImage.readyState === 4) {
+            let imagePlaceholder = duplicate.querySelector(".post__image");
+            imagePlaceholder.style.background = "url("+requestImage.responseURL+")";
+            duplicate.style.display = "flex";
+
+          }
+        };
+      });
+      
     }
   };
+
+
+  
 };
 
-makeBlocks(3);
+makeBlocks();
