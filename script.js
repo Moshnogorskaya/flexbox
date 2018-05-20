eccd5626b5eefc685f291191851682117fad52a3
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-20 19:02:01 +0300

    ready

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const color = function changeColorOfSomeHeadings(elements, newColor) {
   function changeColor(index) {
-    const heading = elements[index].querySelector('.post__color-block');
+    const heading = elements[index].querySelector(".post__color-block");
     heading.style.background = newColor;
   }
   changeColor(0);
@@ -14,48 +14,51 @@ const makeBlocks = function generateBlocksWithDataFromExtSource(paras = 4) {
   if (paras <= 0) return;
 
   // Generate required amount of empty blocks
-  const postTemplate = document.querySelector('.post_half');
-  const postContainer = document.querySelector('.posts');
+  const postTemplate = document.querySelector(".post_half");
+  const postContainer = document.querySelector(".posts");
   for (let i = 0; i < paras; i += 1) {
     const duplicate = postTemplate.cloneNode(true);
-    duplicate.classList.add('duplicate');
+    duplicate.classList.add("duplicate");
     postContainer.insertBefore(duplicate, null);
-    duplicate.classList.add('visible');
+    duplicate.classList.add("visible");
   }
-  const duplicates = postContainer.querySelectorAll('.duplicate');
-  color(duplicates, '#FFC740');
+  const duplicates = postContainer.querySelectorAll(".duplicate");
+  color(duplicates, "#FFC740");
 
-  // Insert text into blocks
-  const requestText = new XMLHttpRequest();
-  const urlText = `https://baconipsum.com/api/?type=all-meat&paras=${paras}`;
-  requestText.open('GET', urlText);
-  requestText.send();
-  requestText.onreadystatechange = () => {
-    if (requestText.readyState === 4) {
-      const paragraphs = JSON.parse(requestText.responseText);
-      duplicates.forEach((duplicate, i) => {
-        const textPlaceholder = duplicate.querySelector('.post__main-text');
-        textPlaceholder.innerHTML = paragraphs[i];
-
-        // Insert Images
-        const requestImage = new XMLHttpRequest();
-        const urlImage = 'https://picsum.photos/200/?random';
-        requestImage.open('GET', urlImage);
-        requestImage.send();
-        requestImage.onreadystatechange = () => {
-          if (requestImage.readyState === 4) {
-            const imagePlaceholder = duplicate.querySelector('.post__image');
-            imagePlaceholder.style.background = `url('${
-              requestImage.responseURL
-            }')`;
-            imagePlaceholder.style.backgroundSize = 'cover';
-
-            // Make blocks visible
-          }
-        };
-      });
-    }
+  let request = urlT => {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      request.open("GET", urlT);
+      request.onload = () => {
+        if (request.status >= 200 && request.status < 300) {
+          resolve(request);
+        } else {
+          reject(request.statusText);
+        }
+      };
+      request.onerror = () => reject(request.statusText);
+      request.send();
+    });
   };
+
+  const urlText = `https://baconipsum.com/api/?type=all-meat&paras=${paras}`;
+  const urlImage = "https://picsum.photos/200/?random";
+
+  request(urlText).then(response => {
+    const paragraphs = JSON.parse(response.responseText);
+    duplicates.forEach((duplicate, i) => {
+      const textPlaceholder = duplicate.querySelector(".post__main-text");
+      textPlaceholder.innerHTML = paragraphs[i];
+    });
+  });
+
+  duplicates.forEach(duplicate => {
+    request(urlImage).then(response => {
+      const imagePlaceholder = duplicate.querySelector(".post__image");
+      imagePlaceholder.style.background = `url('${response.responseURL}')`;
+      imagePlaceholder.style.backgroundSize = "cover";
+    });
+  });
 };
 
-makeBlocks(5);
+makeBlocks(8);
