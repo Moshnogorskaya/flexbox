fdb5e7d7657919dd6cc5a33dd654006fa4d2230a
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-21 09:49:35 +0300

    ready

--- script.js
+++ script.js
@@ -25,38 +25,68 @@ const makeBlocks = function generateBlocksWithDataFromExtSource(paras = 4) {
   const duplicates = postContainer.querySelectorAll('.duplicate');
   color(duplicates, '#FFC740');
 
-  const request = urlT => new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.open('GET', urlT);
-    req.onload = () => {
-      if (req.status >= 200 && req.status < 300) {
-        resolve(req);
-      } else {
-        reject(req.statusText);
-      }
-    };
-    req.onerror = () => reject(request.statusText);
-    req.send();
-  });
+  const request = urlT =>
+    new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest();
+      req.open('GET', urlT);
+      req.onload = () => {
+        if (req.status >= 200 && req.status < 300) {
+          resolve(req);
+        } else {
+          reject(req.statusText);
+        }
+      };
+      req.onerror = () => reject(request.statusText);
+      req.send();
+    });
 
   const urlText = `https://baconipsum.com/api/?type=all-meat&paras=${paras}`;
   const urlImage = 'https://picsum.photos/200/?random';
 
-  request(urlText).then((response) => {
-    const paragraphs = JSON.parse(response.responseText);
-    duplicates.forEach((duplicate, i) => {
-      const textPlaceholder = duplicate.querySelector('.post__main-text');
-      textPlaceholder.innerHTML = paragraphs[i];
-    });
-  });
+  async function insertText(url) {
+    try {
+      const response = await request(url);
+      const paragraphs = JSON.parse(response.responseText);
+      duplicates.forEach((duplicate, i) => {
+        const textPlaceholder = duplicate.querySelector('.post__main-text');
+        textPlaceholder.innerHTML = paragraphs[i];
+      });
+    } catch (err) {
+      console.log('request text failed', err);
+    }
+  }
+  insertText(urlText);
 
-  duplicates.forEach((duplicate) => {
-    request(urlImage).then((response) => {
-      const imagePlaceholder = duplicate.querySelector('.post__image');
+  async function insertImage(url, placeholder) {
+    try {
+      const response = await request(url);
+      const imagePlaceholder = placeholder.querySelector('.post__image');
       imagePlaceholder.style.background = `url('${response.responseURL}')`;
       imagePlaceholder.style.backgroundSize = 'cover';
-    });
+    } catch (err) {
+      console.log('request image failed', err);
+    }
+  }
+
+  duplicates.forEach((duplicate) => {
+    insertImage(urlImage, duplicate);
   });
+
+  // request(urlText).then((response) => {
+  //   const paragraphs = JSON.parse(response.responseText);
+  //   duplicates.forEach((duplicate, i) => {
+  //     const textPlaceholder = duplicate.querySelector('.post__main-text');
+  //     textPlaceholder.innerHTML = paragraphs[i];
+  //   });
+  // });
+
+  // duplicates.forEach((duplicate) => {
+  //   request(urlImage).then((response) => {
+  //     const imagePlaceholder = duplicate.querySelector('.post__image');
+  //     imagePlaceholder.style.background = `url('${response.responseURL}')`;
+  //     imagePlaceholder.style.backgroundSize = 'cover';
+  //   });
+  // });
 };
 
 makeBlocks(8);
