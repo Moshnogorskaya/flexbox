7f88395f4b14ad7ab89fe9c171a5be2e1b4fbdbd
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-21 10:05:10 +0300

    polished

--- script.js
+++ script.js
@@ -25,37 +25,50 @@ const makeBlocks = function generateBlocksWithDataFromExtSource(paras = 4) {
   const duplicates = postContainer.querySelectorAll('.duplicate');
   color(duplicates, '#FFC740');
 
-  const request = urlT => new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.open('GET', urlT);
-    req.onload = () => {
-      if (req.status >= 200 && req.status < 300) {
-        resolve(req);
-      } else {
-        reject(req.statusText);
-      }
-    };
-    req.onerror = () => reject(request.statusText);
-    req.send();
-  });
+  const request = urlT =>
+    new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest();
+      req.open('GET', urlT);
+      req.onload = () => {
+        if (req.status >= 200 && req.status < 300) {
+          resolve(req);
+        } else {
+          reject(req.statusText);
+        }
+      };
+      req.onerror = () => reject(request.statusText);
+      req.send();
+    });
 
   const urlText = `https://baconipsum.com/api/?type=all-meat&paras=${paras}`;
   const urlImage = 'https://picsum.photos/200/?random';
 
-  request(urlText).then((response) => {
-    const paragraphs = JSON.parse(response.responseText);
-    duplicates.forEach((duplicate, i) => {
-      const textPlaceholder = duplicate.querySelector('.post__main-text');
-      textPlaceholder.innerHTML = paragraphs[i];
-    });
-  });
+  function insertText(url) {
+    return request(url)
+      .then((response) => {
+        const paragraphs = JSON.parse(response.responseText);
+        duplicates.forEach((duplicate, i) => {
+          const textPlaceholder = duplicate.querySelector('.post__main-text');
+          textPlaceholder.innerHTML = paragraphs[i];
+        });
+      })
+      .catch(err => console.log('request text failed', err));
+  }
+
+  function insertImage(url, placeholder) {
+    return request(url)
+      .then((response) => {
+        const imagePlaceholder = placeholder.querySelector('.post__image');
+        imagePlaceholder.style.background = `url('${response.responseURL}')`;
+        imagePlaceholder.style.backgroundSize = 'cover';
+      })
+      .catch(err => console.log('request image failed', err));
+  }
+
+  insertText(urlText);
 
   duplicates.forEach((duplicate) => {
-    request(urlImage).then((response) => {
-      const imagePlaceholder = duplicate.querySelector('.post__image');
-      imagePlaceholder.style.background = `url('${response.responseURL}')`;
-      imagePlaceholder.style.backgroundSize = 'cover';
-    });
+    insertImage(urlImage, duplicate);
   });
 };
 
